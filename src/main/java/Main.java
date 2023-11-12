37308922ceb69ff807a597c74edfaa875ffe60f5
oldiright <oliynuk_Dima@ukr.net>
2023-11-12 16:49:12 +0200

    last commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,16 @@
+import com.google.gson.Gson;
+
+import com.google.gson.GsonBuilder;
+import dto.User;
+
 public class Main {
+
+    public static void main(String[] args) {
+        User person = new User("Dmytro", "Oliinyk");
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String result = gson.toJson(person);
+
+        System.out.println(result);
+    }
 }
+
